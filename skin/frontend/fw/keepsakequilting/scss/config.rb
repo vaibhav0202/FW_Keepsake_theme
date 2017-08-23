17eca7fc591f54d98d13c95750068cb05513d72c
vaibhav0202 <vaibhav.s@cisinlabs.com>
2017-08-23 14:31:30 +0530

    fw keepsake theme

---
+++ skin/frontend/fw/keepsakequilting/scss/config.rb
@@ -0,0 +1,13 @@
+http_path = "/skin/frontend/fw/keepsakequilting/"
+css_dir = "../css"
+sass_dir = "."
+sass_path = File.expand_path("..", __FILE__)
+images_dir = "../images"
+javascripts_dir = "../js"
+relative_assets = true
+
+add_import_path "../../default/scss/"
+add_import_path "../../../rwd/default/scss/"
+
+output_style = :compressed
+environment = :production
